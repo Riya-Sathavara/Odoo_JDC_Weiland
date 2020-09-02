93b7e9d23ff5afe892c7457ff25888c58438e156
Riya <riya@aktivsoftware.com>
2020-09-02 18:16:53 +0530

    [ADD] Account Child Parent in v13

---
+++ account_child_parent_COA/models/account_account.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import fields, models
+
+
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    parent_id = fields.Many2one('account.account', string="Parent Account")
